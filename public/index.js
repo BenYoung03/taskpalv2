c10c5ca95d39d01cb8754420e6111f378108274a
Benjamin Young <bryoung1@lakeheadu.ca>
2024-11-02 14:38:03 -0400

    Deploy website and add firebase connections in seperate js file for now

---
+++ public/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA8VdP3Tt4egN2yq8Ps8VKNMiOYKFcGEHQ",
+    authDomain: "taskpal-75c5d.firebaseapp.com",
+    projectId: "taskpal-75c5d",
+    storageBucket: "taskpal-75c5d.firebasestorage.app",
+    messagingSenderId: "257006037090",
+    appId: "1:257006037090:web:871f22788458435c4bc93b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
