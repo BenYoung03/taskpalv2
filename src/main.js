260eda8e0cdd19dad9ba4de75ab28e1cecd93700
Benjamin Young <bryoung1@lakeheadu.ca>
2024-11-02 14:24:35 -0400

    Add firebase hosting elements

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA8VdP3Tt4egN2yq8Ps8VKNMiOYKFcGEHQ",
+    authDomain: "taskpal-75c5d.firebaseapp.com",
+    projectId: "taskpal-75c5d",
+    storageBucket: "taskpal-75c5d.firebasestorage.app",
+    messagingSenderId: "257006037090",
+    appId: "1:257006037090:web:871f22788458435c4bc93b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 document.querySelector(".add-task-confirm").addEventListener('click', function() {
     const inputTask = document.querySelector(".form-container input[type='text']");
     const inputDate = document.querySelector(".form-container input[type='date']");
