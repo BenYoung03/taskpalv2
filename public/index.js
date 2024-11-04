99fba4988623d1e8a91de75b74560a31d0e69c53
Benjamin Young <bryoung1@lakeheadu.ca>
2024-11-03 19:07:56 -0500

    Remove unnecessary code

--- public/index.js
+++ public/index.js
@@ -20,65 +20,64 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 //TODO: Place index.js and main.js in src folder and get them to still work
+//TODO: Add a sign out button
+//TODO: Fix sign in button
 
-document.addEventListener('DOMContentLoaded', function() {
-    const signUp = document.getElementById("sign-up-confirm");
-    if (signUp) {
-        signUp.addEventListener('click', function() {
-            const email = document.getElementById("email").value;
-            const password = document.getElementById("password").value;
-            createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                // Signed up 
-                const user = userCredential.user;
-                console.log(user);
-                window.location.href = "index.html";
-                // ...
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                // ..
-            });
+const signUp = document.getElementById("sign-up-confirm");
+if (signUp) {
+    signUp.addEventListener('click', function() {
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;
+            console.log(user);
+            window.location.href = "index.html";
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
         });
-    } else {
-        console.error("Element with class 'sign-up-confirm' not found.");
-    }
-
-    const signIn = document.getElementById("sign-in-confirm");
-    if (signIn) {
-        signIn.addEventListener('click', function() {
-            alert("Sign in button clicked");
-            const email = document.getElementById("email").value;
-            const password = document.getElementById("password").value;
-            signInWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                // Signed up 
-                const user = userCredential.user;
-                console.log(user);
-                window.location.href = "index.html";
-                // ...
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                // ..
-            });
-        });
-    } else {
-        console.error("Element with class 'sign-in-confirm' not found.");
-    }
-
-    onAuthStateChanged(auth, (user) => {
-        if (user) {
-          // User is signed in, see docs for a list of available properties
-          // https://firebase.google.com/docs/reference/js/auth.user
-            const uid = user.uid;
-            console.log(uid);
-          // ...
-        } else {
-          // User is signed out
-          // ...
-        }
     });
+} else {
+    console.error("Element with id 'sign-up-confirm' not found.");
+}
+
+const signIn = document.getElementById("sign-in-confirm");
+if (signIn) {
+    signIn.addEventListener('click', function() {
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            console.log(user);
+            window.location.href = "index.html";
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+        });
+    });
+} else {
+    console.error("Element with id 'sign-in-confirm' not found.");
+}
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const uid = user.uid;
+        console.log(uid);
+        // ...
+    } else {
+        // User is signed out
+        // ...
+    }
 });
